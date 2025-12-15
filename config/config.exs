f22d98562f21e12b8443b467e55cd299f9147eb0
Shiva Bhusal <hotline.shiva@gmail.com>
2025-12-15 16:38:07 +0545

    Updates scopes of fb login

--- config/config.exs
+++ config/config.exs
@@ -93,7 +93,7 @@ config :ueberauth, Ueberauth,
     facebook:
       {Ueberauth.Strategy.Facebook,
        [
-         default_scope: "email,public_profile,pages_show_list,pages_manage_posts"
+         default_scope: "email,public_profile,user_link,user_posts"
        ]},
     hubspot:
       {Ueberauth.Strategy.Hubspot,
